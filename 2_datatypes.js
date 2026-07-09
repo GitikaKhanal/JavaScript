bdd23016e39faaa0dc402d8237fc6ff924789589
“GitikaKhanal” <gitikakhanal@gmail.com>
2026-07-09 23:05:37 +0545

    datatypes explained

---
+++ 2_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code as newer version
+//alert (3+3) // it will work on browser but in nodejs it wont work
+
+console.log("3+3");//code readability should be high
+console.log("rift");
+let name = "rift" //string
+let age = 19 //number or integer
+let isLoggedIn = true //boolean
+
+// PRIMITIVE DATATYPES
+//number => 2 to the power 53 samma 
+//bigint
+//string => ""
+//boolean => true/false 
+//null => standaline/ intentionally saying it is empty
+//undefined => value not given / not defined
+//symbol => unique
+
+// OBJECT
+
+console.log(typeof "rift");// answer will be string
+console.log(typeof 19 ); // answer will be number
+console.log(typeof null);// answer will be object
+console.log(typeof undefined);//answer will be undefined
+
+
+
+
+
