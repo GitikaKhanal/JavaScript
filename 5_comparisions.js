16916e5655a607994a458dfbc171b78be3ac37e1
“GitikaKhanal” <gitikakhanal@gmail.com>
2026-07-10 19:31:18 +0545

    operations of datatypes

---
+++ 5_comparisions.js
@@ -0,0 +1,29 @@
+console.log(2>1);   //true
+console.log(2>=1);  //true
+console.log(2<1);   //false
+console.log(2<=1);  //false
+console.log(2==1);  //false
+console.log(2!=1);  //true
+
+// console.log("2">1); //true but not recomended
+// console.log("02">1); // true but not recomended       
+
+console.log(null>0);    //false
+console.log(null==0);   //false
+console.log(null>=0);   //true
+
+// undefined also false comparing it with number
+
+//===
+console.log("2"=== 2); // false
+
+
+
+
+
+
+
+
+
+
+
