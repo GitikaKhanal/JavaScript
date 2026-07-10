b37f5fca40ae5beff9f38b7b21a986a62f577d9f
“GitikaKhanal” <gitikakhanal@gmail.com>
2026-07-10 19:08:18 +0545

    operations of datatypes

---
+++ 4_operations.js
@@ -0,0 +1,36 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);// output negative value
+/*
+console.log(2+3);
+console.log(2*2);
+console.log(2-2);
+console.log(2/2);
+console.log(3%2);
+*/
+
+let str1 = "Hello,"
+let str2 = "rift"
+let str3 = str1 + str2 
+// console.log(str3);  // output ->hello,rift
+// console.log("1"+2); // output ->12
+// console.log(1+"2"); //  output ->12
+// console.log("1"+2+2); //  output ->122
+// console.log(1+2+"2"); //  output ->32
+// console.log((3+4) * 5 % 3); //  output ->2
+
+console.log(+true); // true= true , +true= 1
+console.log(+""); // 0
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+let gameCounter = 100
+gameCounter++;//postfix
+console.log(gameCounter);// output -> 101
+
+//prefix-> ++a
+//postfix-> a++
+
+
+
+
+
